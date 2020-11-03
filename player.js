b14b0400c59fd6e6ab80b7dc575c5f04b8f641b2
eric2523@gmail.com <eric2523@gmail.com>
2020-11-02 16:44:49 -0800

    Add left right collision detection

--- player.js
+++ player.js
@@ -5,7 +5,8 @@ export class Player {
     this.yPos = 0
     this.width = 50;
     this.height = 50;
-    this.velocity = 20
+    // velocity needs to be changed later based on canvas size or vice versa
+    this.velocity = 10;
     this.velocityX = 0;
     this.velocityY = 0;
     this.ctx = canvas.getContext("2d")
@@ -13,7 +14,19 @@ export class Player {
     this.moveLeft = this.moveLeft.bind(this)
     this.moveDown = this.moveDown.bind(this)
     this.moveUp = this.moveUp.bind(this)
+    this.checkBorderCollision = this.checkBorderCollision.bind(this)
   }
+
+  checkBorderCollision(){
+    if (this.xPos <= 0){
+      this.velocityX = 0
+      this.xPos = 0
+    } else if ((this.xPos + this.width) > this.canvas.width) {
+      this.velocityX -= this.velocity
+      this.xPos = this.velocityX
+    }
+  }
+
 
   moveLeft(){
     this.velocityX -= this.velocity
@@ -36,6 +49,7 @@ export class Player {
   }
 
   draw(){
+    this.checkBorderCollision()
     this.ctx.fillStyle = "blue"
     // this.ctx.rect(0,0, 50, 50)
     this.ctx.fillRect(this.xPos, this.yPos, this.width, this.height)
