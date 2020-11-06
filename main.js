ce2d201cec58c3a70df057bbaea41ab0737494a1
eric2523@gmail.com <eric2523@gmail.com>
2020-11-06 10:37:30 -0800

    Remove debugger

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
-import { Engine } from "./engine.js"
+import { Engine } from "./engine.js";
 
-const canvas = document.getElementById("myCanvas")
+const canvas = document.getElementById("myCanvas");
 
 window.addEventListener("DOMContentLoaded", () => {
   // initialize game
-  const engine = new Engine(60, canvas)
-  
+  const engine = new Engine(60, canvas);
+
   engine.start("loading-screen");
-  
-  const startGame = document.getElementById("start-game")
-  const home = document.getElementById("home")
-  const pause = document.getElementById("pause")
-  const play = document.getElementById("play")
-  const restart = document.getElementById("restart")
+
+  const startGame = document.getElementById("start-game");
+  const home = document.getElementById("home");
+  const pause = document.getElementById("pause");
+  const play = document.getElementById("play");
+  const restart = document.getElementById("restart");
 
   startGame.addEventListener("click", () => {
-    debugger
-    startGame.classList.add("hide")
+    startGame.classList.add("hide");
     engine.restartGame();
     engine.start();
-  })
+  });
 
   home.addEventListener("click", () => {
-    startGame.classList.remove("hide")
+    startGame.classList.remove("hide");
     engine.stop();
-    engine.start("loading-screen")
-  })
+    engine.start("loading-screen");
+  });
 
   restart.addEventListener("click", () => {
     engine.restartGame();
     engine.start();
-  })
+  });
 
   pause.addEventListener("click", () => {
-    engine.stop()
-  })
+    engine.stop();
+  });
 
   play.addEventListener("click", () => {
     engine.run();
-  })
-  
+  });
+
   window.addEventListener("keydown", () => {
-    engine.controls.handleKeyDown()
-  })
+    engine.controls.handleKeyDown();
+  });
 
   window.addEventListener("keyup", () => {
-    engine.controls.handleKeyUp()
-  })
-
-})
+    engine.controls.handleKeyUp();
+  });
+});
