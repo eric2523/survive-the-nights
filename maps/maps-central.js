f17796128babd4bb0d2ad2f3ae1ea2a30c598f29
eric2523@gmail.com <eric2523@gmail.com>
2020-11-06 15:06:00 -0800

    Add level 3

--- maps/maps-central.js
+++ maps/maps-central.js
@@ -1,5 +1,6 @@
 import { mapbase1 } from "./01-mapbase.js"
 import { mapbase2 } from "./02-mapbase.js"
+import { mapbase3 } from "./03-mapbase.js"
 
 export const allMaps = {
   1: {
@@ -8,6 +9,10 @@ export const allMaps = {
   },
   2: {
     map: mapbase2,
+    zombieCount: 3
+  },
+  3: {
+    map: mapbase3,
     zombieCount: 5
   }
 }
