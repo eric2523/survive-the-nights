e2083835f2b107770fd100bffea598e695d2ba63
eric2523@gmail.com <eric2523@gmail.com>
2020-11-17 11:22:51 -0800

    Add music and mute buttons

--- main.js
+++ main.js
@@ -5,8 +5,31 @@ const canvas = document.getElementById("myCanvas");
 window.addEventListener("DOMContentLoaded", () => {
   // initialize game
   const engine = new Engine(60, canvas);
-
   engine.start("loading-screen");
+
+  // audio files
+  let backgroundMusic = new Audio();
+  backgroundMusic.src =
+    "https://raw.githubusercontent.com/eric2523/survive-the-nights/main/sound/2020-03-22_-_A_Bit_Of_Hope_-_David_Fesliyan.mp3";
+
+  backgroundMusic.loop = true;
+  backgroundMusic.volume = 0.25;
+
+  const muteSound = document.getElementById("mute-sound");
+
+  let playSound = false;
+  muteSound.addEventListener("click", () => {
+    if (backgroundMusic.paused) {
+      debugger
+      muteSound.innerText = "Mute"
+      playSound = true;
+      backgroundMusic.play();
+    } else {
+      muteSound.innerText = "Unmute"
+      playSound = false;
+      backgroundMusic.pause();
+    }
+  });
 
   const startGame = document.getElementById("start-game");
   const home = document.getElementById("home");
@@ -15,21 +38,21 @@ window.addEventListener("DOMContentLoaded", () => {
   const restart = document.getElementById("restart");
   const nextLevel = document.getElementById("next-level-btn");
   const playAgain = document.getElementById("play-again-btn");
-  const instructions = document.getElementsByClassName("instructions")[0]
-  const loadingScreen = document.getElementsByClassName("loading-screen")[0]
+  const instructions = document.getElementsByClassName("instructions")[0];
+  const loadingScreen = document.getElementsByClassName("loading-screen")[0];
 
   playAgain.addEventListener("click", () => {
     playAgain.classList.add("hide");
     engine.restartGame();
     engine.running = true;
     engine.start();
-  })
-  
+  });
+
   nextLevel.addEventListener("click", () => {
-    nextLevel.classList.add("hide")
+    nextLevel.classList.add("hide");
     engine.game.createLevel(engine.game.level);
     engine.run();
-  })
+  });
 
   startGame.addEventListener("click", () => {
     // instructions.classList.add("hide");
@@ -38,17 +61,19 @@ window.addEventListener("DOMContentLoaded", () => {
     engine.restartGame();
     engine.running = true;
     engine.start();
+    if (playSound){
+      backgroundMusic.play();
+    }
   });
 
   home.addEventListener("click", () => {
     nextLevel.classList.add("hide");
-    // instructions.classList.remove("hide");
     playAgain.classList.add("hide");
-    // startGame.classList.remove("hide");
     loadingScreen.classList.remove("hide");
     engine.stop();
     engine.running = true;
     engine.start("loading-screen");
+    backgroundMusic.pause();
   });
 
   restart.addEventListener("click", () => {
@@ -61,16 +86,20 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   pause.addEventListener("click", () => {
-    if (engine.running){
+    if (engine.running) {
       engine.running = false;
       engine.stop();
+      backgroundMusic.pause();
     }
   });
 
   play.addEventListener("click", () => {
-    if (!engine.running){
+    if (!engine.running) {
       engine.running = true;
       engine.run();
+      if (playSound){
+        backgroundMusic.play();
+      }
     }
   });
 
