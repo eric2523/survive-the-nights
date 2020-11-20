94c9cd99f9906c024bae391cf68c8837c0ba374d
eric2523@gmail.com <eric2523@gmail.com>
2020-11-20 14:51:37 -0800

    Add new map

--- maps/maps-central.js
+++ maps/maps-central.js
@@ -1,6 +1,7 @@
 import { mapbase1 } from "./01-mapbase.js"
 import { mapbase2 } from "./02-mapbase.js"
 import { mapbase3 } from "./03-mapbase.js"
+import { mapbase4 } from "./04-mapbase.js"
 
 export const allMaps = {
   1: {
@@ -17,5 +18,10 @@ export const allMaps = {
     map: mapbase3,
     zombieCount: 10,
     spawnRate: 1500
+  },
+  4: {
+    map: mapbase4,
+    zombieCount: 30,
+    spawnRate: 1250
   }
 }
